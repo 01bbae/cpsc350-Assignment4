25d1e95b183b4c0ab6c7b46ae7fdbf7aa729eada
BJ Bae <01bbae@gmail.com>
2020-04-14 12:01:02 +0000

    List Node implemented

---
+++ DoublyLinked.h
@@ -0,0 +1,12 @@
+#include "ListNode.h"
+
+class DoublyLinked{
+public:
+  DoublyLinked();
+  DoublyLinked(int s);
+  ~DoublyLinked();
+  void insertFront(int d);
+  void insertAtPos(int d);
+  int removeAtFront();
+
+}
